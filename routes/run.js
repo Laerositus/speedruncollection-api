5ad6fba20cd69b5a775c6ab4baba89d69c8ff191
Laerositus <jelleboy22@gmail.com>
2021-11-28 00:57:33 +0100

    Included postman tests in repo

--- routes/run.js
+++ routes/run.js
@@ -5,7 +5,7 @@ var router = express.Router();
 
 router.post("/", RunController.runStore);
 router.get("/:id", RunController.runDetail);
-router.put("/:id", RunController.runUpdate);
+// router.put("/:id", RunController.runUpdate);
 router.delete("/:id", RunController.runDelete);
 
 module.exports = router;
